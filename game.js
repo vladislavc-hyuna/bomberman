da5349f76816fc5997021aa5bb9b241c3681605f
Vladislav Cherepenya <vladislav.cherepenya@hyuna.bb>
2015-10-27 14:39:16 +0200

    add multiplayer

--- game.js
+++ game.js
@@ -6,6 +6,47 @@ function Game()
 Game.prototype.init = function() {
 	var map = new Map();
 	map.init();
+
+	socket = io.connect();
+
+	$('body').on('keydown', doKeyDown);
+
+	function doKeyDown(e){
+		
+		if(e.keyCode == 87){
+			socket.emit('move', {'id':socket.id, 'direction':'up'});
+		}
+		if(e.keyCode == 83){
+			socket.emit('move', {'id':socket.id, 'direction':'down'});
+		}
+		if(e.keyCode == 65){
+			socket.emit('move', {'id':socket.id, 'direction':'left'});
+		}
+		if(e.keyCode == 68){
+			socket.emit('move', {'id':socket.id, 'direction':'right'});
+		}
+
+	}
+
+	socket.on('move', function(data){
+		//console.log(data);
+		clearCanvas();
+			
+		map.buildWalls();
+
+		for(var i = 0; i < data.length; i++)
+		{
+			map.ctx.fillRect(data[i].x, data[i].y, 40, 40);
+		}
+
+
+	});
+
+	function clearCanvas()
+	{
+		map.canvas.width = map.canvas.width;
+	}
+	
 }
 
 var game = new Game();
---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require("express"),
+    app = express(),
+    server = require("http").createServer(app),
+    io = require("socket.io").listen(server),
+	players = [];
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.use(express.static(__dirname + '/'));
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	console.log(socket.id + ' is connected');
+	 socket.on('disconnect', function(){
+         for (var i = 0; i < players.length; i++) {
+            if (players[i].id == socket.id) {
+                players.splice(i, 1);
+                console.log(socket.id + ' is disconnected');
+            }
+        }
+    });
+	players.push({
+		id:socket.id,
+		x:0,
+		y:0,
+	});
+
+	io.sockets.emit('move', players);
+
+	socket.on('move', function(data){
+		var player = getPlayerByID(socket.id);
+		if(data.direction == 'up'){
+			player.y -= 40;
+		}else if(data.direction == 'down'){
+			player.y += 40;
+		}else if(data.direction == 'left'){
+			player.x -= 40;
+		}else if(data.direction == 'right'){
+			player.x += 40;
+		}
+
+		io.sockets.emit("move", players);
+	});
+
+	function getPlayerByID(id) {
+	    for (var i = 0; i < players.length; i++) {
+	        if (id == players[i].id) {
+	            return players[i];
+	        }
+	    }
+	}	
+
+
+
+});
+
